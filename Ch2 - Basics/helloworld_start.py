30728ff0493dfda20b423c7996e73e522386a3b9
Meir Sedlis <meirsedlis@gmail.com>
2022-10-18 12:34:28 -0500

    define main in helloworld_start and write check for execution vs import

--- Ch2 - Basics/helloworld_start.py
+++ Ch2 - Basics/helloworld_start.py
@@ -3,4 +3,11 @@
 # LinkedIn Learning Python course by Joe Marini
 #
 
+def main():
+    print("Hello World!")
+    name = input("What is your name? ")
+    print("Nice to meet you", name)
 
+# checking to see that the program has been executed, prevents the file from being run on import
+if __name__ == "__main__":
+    main()
